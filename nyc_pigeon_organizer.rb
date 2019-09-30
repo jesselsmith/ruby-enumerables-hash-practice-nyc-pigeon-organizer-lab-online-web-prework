482f40e2f2cd7ded3da9de609cd50ae9efd6f00a
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-30 03:46:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,10 @@ def nyc_pigeon_organizer(data)
     value.each_pair do |(subcategory, array)|
       array.each do |bird_name|
         if pigeon_list.has_key? bird_name && pigeon_list[bird_name].has_key? category
+          pigeon_list[category] << subcategory.to_s
+        elsif pigeon_list.has_key? bird_name 
+          
+        end
           
       end
     end
