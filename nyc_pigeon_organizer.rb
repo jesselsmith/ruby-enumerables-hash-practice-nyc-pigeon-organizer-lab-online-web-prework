e6ed590d66a9c62fb66489326928afe6e92728bf
Jesse Smith <jesse.lee.smith@gmail.com>
2019-09-30 03:53:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,14 @@ def nyc_pigeon_organizer(data)
   organized_data = data.reduce({}) do |pigeon_list, (category, value)|
     value.each_pair do |(subcategory, array)|
       array.each do |bird_name|
-        if pigeon_list.has_key? bird_name && pigeon_list[bird_name].has_key? category
-          pigeon_list[category] << subcategory.to_s
-        elsif pigeon_list.has_key? bird_name 
+        if !pigeon_list.has_key? bird_name
+          pigeon_list
+        elsif ( (pigeon_list.has_key? bird_name) && (!pigeon_list[bird_name].has_key? category) )
+          pigeon_list[category] = [subcategory.to_s]
+        else
           
         end
-          
+          pigeon_list[bird_name][category] << subcategory.to_s
       end
     end
   end
